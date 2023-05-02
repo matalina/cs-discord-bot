a98af7baba402d53b66f3682e003c001294c474d
Alicia Wilkerson <matalina@gmail.com>
2023-05-02 13:29:29 -0500

    updated inspire

--- src/commands/generic/inspire.js
+++ src/commands/generic/inspire.js
@@ -1,10 +1,18 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {SlashCommandBuilder} = require('discord.js');
+const {getRandomKeywords} = require('../../lib/keywords.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('inspire')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+  data: new SlashCommandBuilder()
+    .setName('inspire')
+    .setDescription('Generate some inspiration')
+    .addIntegerOption((option) => {
+      return option
+        .setName('count')
+        .setDescription('how many words do you want?');
+    }),
+  async execute(interaction) {
+    const count = interaction.options.getInteger('count') ?? 3;
+    const keywords = getRandomKeywords(count);
+    await interaction.reply(keywords.join(' ,'));
+  },
 };
