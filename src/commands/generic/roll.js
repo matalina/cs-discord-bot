255ba648c8afc6b748f2e4fcca5d4194dc0ad50f
Alicia Wilkerson <matalina@gmail.com>
2023-05-02 10:55:05 -0500

    updated /roll

--- src/commands/generic/roll.js
+++ src/commands/generic/roll.js
@@ -1,18 +1,22 @@
-const { DiceRoll } = require('@dice-roller/rpg-dice-roller');
-const { SlashCommandBuilder } = require('discord.js');
+const {DiceRoll} = require('@dice-roller/rpg-dice-roller');
+const {SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('roll')
-		.setDescription('Replies with Pong!')
-		.addStringOption((option) => {
-			return option
-				.setName('dice')
-				.setDescription('dice forumal to roll')
-		}),
-	async execute(interaction) {
-		const dice = interaction.options.getString('dice') ?? '1d20';
-		const roll = new DiceRoll(dice);
-		await interaction.reply(`Rolling ${dice}!\n${roll.output}`);
-	},
+  data: new SlashCommandBuilder()
+    .setName('roll')
+    .setDescription(
+      'Rolls those bones! [dice notation](https://dice-roller.github.io/documentation/guide/notation/)'
+    )
+    .addStringOption((option) => {
+      return option
+        .setName('dice')
+        .setDescription(
+          '[Dice notation](https://dice-roller.github.io/documentation/guide/notation/) to roll'
+        );
+    }),
+  async execute(interaction) {
+    const dice = interaction.options.getString('dice') ?? '1d20';
+    const roll = new DiceRoll(dice);
+    await interaction.reply(`Rolling ${dice}!\n${roll.output}`);
+  },
 };
