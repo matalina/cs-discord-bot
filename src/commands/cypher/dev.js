43d7d14c92c3dcb7415b154707b85dcc767894f5
Alicia Wilkerson <matalina@gmail.com>
2023-05-02 15:06:38 -0500

    updated developer

--- src/commands/cypher/dev.js
+++ src/commands/cypher/dev.js
@@ -3,7 +3,7 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('dev')
-    .setDescription('Replies with Pong!'),
+    .setDescription('Generate a developer'),
   async execute(interaction) {
     const {dev, roll} = rollDeveloper();
     await interaction.reply(`**Developer**\n${dev}\n${roll.output}`);
