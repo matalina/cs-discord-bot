92271591bb2c8a4e5989594ab7712ccdc8c75abb
Alicia Wilkerson <matalina@gmail.com>
2023-05-02 15:23:09 -0500

    added task

--- src/commands/cypher/task.js
+++ src/commands/cypher/task.js
@@ -1,10 +1,53 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {DiceRoll} = require('@dice-roller/rpg-dice-roller');
+const {SlashCommandBuilder} = require('discord.js');
+const {rollDeveloper} = require('../../lib/developer');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('task')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+  data: new SlashCommandBuilder()
+    .setName('task')
+    .setDescription('Make a task check')
+    .addStringOption((option) => {
+      return option
+        .setName('action')
+        .setDescription('what are you trying to do')
+        .setRequired(true);
+    })
+    .addIntegerOption((option) => {
+      return option
+        .setName('target')
+        .setDescription('what is the target value')
+        .setRequired(true);
+    }),
+  async execute(interaction) {
+    const roll = new DiceRoll('1d20');
+    const action = interaction.options.getString('action');
+    const target = interaction.options.getInteger('target');
+    let output = `${action}`;
+    if (roll.total < target) {
+      output += '\n**Fail**';
+    } else {
+      output += '\n**Success**';
+    }
+    if (roll.total === 1) {
+      output += '\n_**GM Intrusion (No XP)**_';
+    }
+    if (roll.total === 17) {
+      output += '\nDamage +1';
+    }
+    if (roll.total === 18) {
+      output += '\nDamage +2';
+    }
+    if (roll.total === 19) {
+      output += '\nDamage +3 or Minor Effect';
+    }
+    if (roll.total === 20) {
+      output += '\nDamage +4 or Major Effect';
+    }
+    output += `\n${roll.output}`;
+
+    const dev = rollDeveloper();
+    output += `\n**Developer**\n${dev.dev}\n${dev.roll.output}`;
+
+    await interaction.reply(output);
+  },
 };
